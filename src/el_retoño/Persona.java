751e6da8555b0fa3b49fba89ea773787f2971c58
MatiasBrT16 <mb162508@gmail.com>
2025-11-23 00:52:34 -0500

    Validación para setTipoDoc y setNumDoc

--- src/el_retoño/Persona.java
+++ src/el_retoño/Persona.java
@@ -53,7 +53,6 @@ abstract class Persona {
     }
 
     public void setTipoDoc(String TipoDoc) {
-<<<<<<< HEAD
         if (TipoDoc == null) {
             this.TipoDoc = null;
             return;
@@ -67,9 +66,6 @@ abstract class Persona {
             System.err.println("Tipo de documento no válido. Debe ser 'DNI', 'CE' o 'PASAPORTE'.");
             this.TipoDoc = null;
         }
-=======
-        this.TipoDoc = TipoDoc;
->>>>>>> 983902f8865ade6d7ea345055f87cecc585b2e6f
     }
 
     public String getNumDoc() {
@@ -77,7 +73,6 @@ abstract class Persona {
     }
 
     public void setNumDoc(String NumDoc) {
-<<<<<<< HEAD
         if (this.TipoDoc == null) {
             System.err.println("Establezca primero un Tipo de Documento válido antes de asignar el Número de Documento.");
             this.NumDoc = null;
@@ -100,9 +95,6 @@ abstract class Persona {
             System.err.println("Número de Documento no válido para el tipo " + this.TipoDoc + ". Debe tener " + longitudEsperada + " dígitos.");
             this.NumDoc = null;
         }
-=======
-        this.NumDoc = NumDoc;
->>>>>>> 983902f8865ade6d7ea345055f87cecc585b2e6f
     }
 
     public String getSexo() {
